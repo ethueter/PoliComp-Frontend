0918436954d41ae54363ae9ea5d36bad7b6aec45
Tyler Hueter <tylerhueter08@gmail.com>
2019-05-24 16:36:50 -0400

    header

---
+++ policomp/src/components/Calls.js
@@ -0,0 +1,17 @@
+// import React from 'react';
+
+// const key = '9663743b34524026854a945d187b6f6e'
+// const poliFilter = '%27Trump%20AND%20Republican%20AND%20Democrat%20AND%20Congress%20AND%20Senate%20AND%20Mueller%20AND%20political%20AND%202020%20AND%20campaign%20AND%20candidate%20AND%20election%27'
+// const domains = 'foxnews.com,huffpost.com,cnn.com,nytimes.com,washingtonpost.com,washingtontimes.com,apnews.com,npr.com,aljazeera.com,brietbart.com,nationalreview.com,drudgereport.com,reuters.com'
+
+
+
+
+// const topStories = () => {
+//     fetch(`https://newsapi.org/v2/everything?q=politics&domains=${domains}&language=en&sortby=publishedAt&pageSize=100&apiKey=${key}`)
+//         // .then(res => res.json())
+//         // .then(articles => {return articles})
+
+// }
+
+// export default topStories
